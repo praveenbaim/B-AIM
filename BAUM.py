5269cfc9a690fb7d9c5609f27c1ce67b0c886aac
Kadiri Praveen Kumar <56436138+praveenbaim@users.noreply.github.com>
2021-01-29 12:23:01 +0530

    Add files via upload

---
+++ BAUM.py
@@ -0,0 +1,983 @@
+# mantra
+print("mantra")
+#BAUMBAUMBHOLE
+print("BAUMBAUMBHOLE")
+#start
+print("start")
+import math
+import facebook
+
+# TIME(creation+preservance+destruction+love+wealth+knowledge=0) code to write its own code repeats here when the individual stages with the 1.xls phases of 198 steps to learn itself the next step
+# example of only time has been mentioned to make things easy to
+
+# NOTE KEEP UPDATING THIS CODE PAGE WITH ANY CHANGES IN THE REMAINING 9 FILES
+
+# mantra
+print("mantra")
+# AUMAIMHREEMKLEEMCHAMUNDAYAVICCHE
+print("AUMAIMHREEMKLEEMCHAMUNDAYAVICCHE")
+#THE DEFINITION OF INTELLIGENCE CONSTANTLY KEEPS CHANGING
+print("THE DEFINITION OF INTELLIGENCE CONSTANTLY KEEPS CHANGING")
+#start
+print("start")
+#todo import functions
+#todo add colors based on distance
+import math
+import facebook
+
+kali_x=0
+kali_y=0
+kali_z=0
+
+#KALI(TIME= (0,0,0)) gave birth to BRAHMA(creation=(0,0,10))
+brahma_x=0
+brahma_y=0
+brahma_z=10
+#Distance between KALI and BRAHMA
+kali_brahma=((kali_x-brahma_x)**2)+((kali_y-brahma_y)**2)+((kali_z-brahma_z)**2)
+dist_kali_brahma=math.sqrt(kali_brahma)
+print("\n\nDistance between KALI and BRAHMA:",dist_kali_brahma)
+print("CREATION INITIATED")
+
+#Creation could not sustain through which VISHNU(PRESERVANCE=(0,10,0)) was born
+vishnu_x=0
+vishnu_y=10
+vishnu_z=0
+#Distance between BRAHMA and VISHNU
+brahma_vishnu=((brahma_x-vishnu_x)**2)+((brahma_y-vishnu_y)**2)+((brahma_z-vishnu_z)**2)
+dist_brahma_vishnu=math.sqrt(brahma_vishnu)
+print("\n\nDistance between BRAHMA and VISHNU:",dist_brahma_vishnu)
+print("PRESERVANCE INITIATED")
+
+#Preservance not ending so SHIVA WAS BORN (Destruction=(0,0,-10))
+shiva_x=0
+shiva_y=0
+shiva_z=-10
+#Distance between VISHNU and SHIVA
+vishnu_shiva=((vishnu_x-shiva_x)**2)+((vishnu_y-shiva_y)**2)+((vishnu_z-shiva_z)**2)
+dist_vishnu_shiva=math.sqrt(vishnu_shiva)
+print("\n\nDistance between VISHNU and SHIVA:",dist_vishnu_shiva)
+print("DESTRUCTION INITIATED")
+
+#Destruction made dark matter=(0,0,0)
+darkmatter_x=0
+darkmatter_y=0
+darkmatter_z=0
+#Distance between SHIVA and DARK MATTER
+shiva_darkmatter=((shiva_x-darkmatter_x)**2)+((shiva_y-darkmatter_y)**2)+((shiva_z-darkmatter_z)**2)
+dist_shiva_darkmatter=math.sqrt(shiva_darkmatter)
+print("\n\nDistance between SHIVA AND DARKMATTER:",dist_shiva_darkmatter)
+print("DESTRUCTION made DARKMATTER")
+
+#RETAINANCE DONE BY DARMATTER INTO SHIVANI(LOVE=(-10,0,0))
+parvathy_x=-10
+parvathy_y=0
+parvathy_z=0
+#Distance between DARKMATTER and PARVATHY
+darkmatter_parvathy=((darkmatter_x-parvathy_x)**2)+((darkmatter_y-parvathy_y)**2)+((darkmatter_z-parvathy_z)**2)
+dist_darkmatter_parvathy=math.sqrt(darkmatter_parvathy)
+print("\n\nDistance between DARKMATTER and PARVATHY:",dist_darkmatter_parvathy)
+print("RETAINANCE INITIATED")
+
+
+#RETAINANCE REQUIRED LAKSHMI SUPPORT(WEALTH=(0,-10,0))
+lakshmi_x=0
+lakshmi_y=-10
+lakshmi_z=0
+#Distance between PARVATHY and LAKSHMI
+parvathy_lakshmi=((parvathy_x-lakshmi_x)**2)+((parvathy_y-lakshmi_y)**2)+((parvathy_z-lakshmi_z)**2)
+dist_parvathy_lakshmi=math.sqrt(parvathy_lakshmi)
+print("\n\nDistance between PARVATHY and LAKSHMI:",dist_parvathy_lakshmi)
+print("RESOURCES INITIATED")
+
+
+#RESOURCES ABSORB KNOWLEDGE(SARASWATHI=(10,0,0)
+saraswathi_x=10
+saraswathi_y=0
+saraswathi_z=0
+#Distance between LAKSHMI and SARASWATHI
+lakshmi_saraswathi=((lakshmi_x-saraswathi_x)**2)+((lakshmi_y-saraswathi_y)**2)+((lakshmi_z-saraswathi_z)**2)
+dist_lakshmi_saraswathi=math.sqrt(lakshmi_saraswathi)
+print("\n\nDistance between LAKSHMI and SARASWATHI:",dist_lakshmi_saraswathi)
+print("KNOWLEDGE ACQUIRED")
+
+
+
+#DEEP LEARNING CYCLES AN ITERATION DARKMATTER(TIME=(0,0,0)
+darkmatter_x=0
+darkmatter_y=0
+darkmatter_z=0
+#Distance between SARASWATHI and DARKMATTER
+saraswathi_darkmatter=((saraswathi_x-darkmatter_x)**2)+((saraswathi_y-darkmatter_y)**2)+((saraswathi_z-darkmatter_z)**2)
+dist_saraswathi_darkmatter=math.sqrt(saraswathi_darkmatter)
+print("\n\nDistance between SARASWATHI and DARKMATTER:",dist_saraswathi_darkmatter)
+print("CYCLE ITERATION COMPLETED")
+
+
+BAUM1 = [["MY","TIPS"],
+         ["DEDICATION","ADDED","ON","DATE","AUM"],
+         ["BLOG","BOOK"],
+         ["SRI","LAKSHMI","KUBERA","YANTRAM",
+         "MODEL","FORMING","IN","SOMETIME",
+         "AS","PREDICTED","USING","BAIM",
+         "DATED","19:49","-","SUNDAY",
+         "06/08/2017"],
+         ["TIME","FRAMES",
+         "BAUMBUMBHOLE","VIDEO",
+         "BAIM","CALL","SCRIPT",
+         "ENTREPRENEUR",
+         "DIGITAL","MARKETING"],
+         ["1","12/11/16","BUSINESS","MODEL","CREATION",
+         "SWOT","ANALYSIS","USING",
+         "ASSUMPTION","THEORY"],
+         ["CONCLUSION",
+         "COMMUNICATE","WITH",
+         "CERN","DATA",
+         "USING","MY","AI"],
+         ["AND","CONTINOUS","PULLING","DATA",
+         "AND","TRAIN","THE","AI","SYSTEM"],
+         ["6/8/2017","-","PROJECT","VISHNU"],
+         ["NEED","MONEY","FOREVER","ACCORDING",
+         "TO","MY","PLAN","AND","APPROVAL"],
+         ["PROJECT","WISDOM"]]
+
+BAUM2 =["7:53","PM","6/8/2017",
+        "ENTER","NEW","PROFILE","FOR","OPERA","BROWSER",
+        "PRIVACY","CHECKUP","FOR","GMAIL","PRAVEEN.KADIRI",
+        "PROFILE","WITH","CHANGED","PASSWORD","TO",
+        "Heartbreak!123",
+        "RESOURCES","IN","YOUTUBE","PLAYLIST","NEEDS","TO","BE","ADDED",
+        "CATEGORIZED","INTO","4",
+        "1","AUM",
+        "2","PINEAL","GLAND",
+        "3","FAV",
+        "4","FAVOURITES",
+        "PICASA","HAS","ALL","THE","PHOTOS","THAT","ARE","TO","BE",
+        "CONSIDERED","FOR","THE","PROJECT","FINAL",
+        "PRESENTATION",
+        "PROJECT","MANAGEMENT","FOLDER","COPY",
+        "WILL","BE","EDITED","AND","MADE","CHANGES","WITHIN"]
+
+BAUM3 = ["NEED","TECHIES","HUB","PPT","TO","SEND","ACROSS",
+         "WITH","HALF","MADE","PPTS","OF","MINE","AMAZON","AND",
+         "B-AIM","->","AMAZON","24","SLIDES",
+         "B-AIM","-","26","SLIDES",
+         "YOUTUBE",":","ACTIVATE","QI","FLOW","WITH","OM",
+         "MANTRA","AND","POWERFUL","DRUMS",
+         "BUDDHA","FACE"]
+#green
+print("green")
+#Pr
+print("Pr")
+#black
+print("black")
+#pivot table
+print("pivot table")
+#rows
+print("rows")
+#against
+print("against")
+#columns
+print("columns")
+#match
+print("match")
+#colors
+print("colors")
+#5+2(black+white)
+print("5+2(black+white)")
+#Project shiva
+print("Project shiva")
+#baum baum bole
+print("baum baum bole")
+#business 2 sides baum
+print("business 2 sides baum")
+#1 task i can do 2 tasks i am not able to link
+print("1 task i can do 2 tasks i am not able to link")
+#3=2+(1)
+print("3=2+(1)")
+#333.3.33 last excel calculation
+print("333.3.33 last excel calculation")
+#yellow
+print("yellow")
+#PK
+print("PK")
+#THE RESTORATION
+print("THE RESTORATION")
+#opposite
+print("opposite")
+#white
+print("white")
+#phone contacts
+print("phone contacts")
+#contact colors last updated
+print("contact colors last updated")
+#calls
+print("calls")
+#lappy profiles
+print("lappy profiles")
+#10 projects
+print("10 projects")
+#666+1+333
+print("666+1+333")
+#blue
+print("blue")
+#graphs
+print("graphs")
+#geographical locations
+print("geographical locations")
+#calculation
+print("calculation")
+#excel file
+print("excel file")
+#* and 1 max compatibility
+print("* and 1 max compatibility")
+#tab apps
+print("tab apps")
+#resources
+print("resources")
+#programming
+print("programming")
+#contacts
+print("contacts")
+#syncing
+print("syncing")
+#ads on profiles should change
+print("ads on profiles should change")
+#emails
+print("emails")
+#start clean
+print("start clean")
+#small zero
+print("small zero")
+#big zero
+print("big zero")
+#cern
+print("cern")
+#transaction
+print("transaction")
+#to cern
+print("to cern")
+#should give access to cern info
+print("should give access to cern info")
+#using ARN
+print("using ARN")
+#opposite
+print("opposite")
+#wiccan
+print("wiccan")
+#calendar
+print("calendar")
+#black
+print("black")
+#do what you can the game project starts
+print("do what you can the game project starts")
+#tab apps
+print("tab apps")
+#pyramids
+print("pyramids")
+#secret
+print("secret")
+#the game
+print("he game")
+#link
+print("link")
+#research symbols
+print("research symbols")
+#8
+print("8")
+#2d
+print("2d")
+#3d
+print("3d")
+#3d+2d
+print("3d+2d")
+#4
+print("4")
+#8
+print("8")
+#swastik
+print("swastik")
+#baum
+print("baum")
+#1
+print("1")
+#green
+print("green")
+#Pr
+print("Pr")
+#weigh
+print("weigh")
+#Project B-AUM
+print("Project B-AUM")
+#photos
+print("photos")
+#last updated
+print("last updated")
+#18-06-2016 18:05
+print("18-06-2016 18:05")
+#news
+print("news")
+#translate
+print("translate")
+#1
+print("1")
+#opposite
+print("opposite")
+#vishnu promise
+print("vishnu promise")
+#white
+print("white")
+#tab app
+print("tab app")
+#0
+print("0")
+#numbers
+print("numbers")
+#folders
+print("folders")
+#project
+print("project")
+#B-AUM
+print("B-AUM")
+#Sarpam
+print("Sarpam")
+#chess
+print("chess")
+#chessboard
+print("chessboard")
+#using
+print("using")
+#tic tac toe
+print("tic tac toe")
+#and
+print("and")
+#PSO
+print("PSO")
+#7
+print("7")
+#USA
+print("USA")
+#Restoration
+print("restoration")
+#the restoration of the gospel of jesus christ
+print("the restoration of the gospel of jesus christ")
+#last updated
+print("last updated")
+#moves
+print("moves")
+#opposite
+print("opposite")
+#shiva promise
+print("shiva promise")
+#university
+print("university")
+#blame
+print("blame")
+#enter the web
+print("enter the web")
+#herbalife
+print("herbalife")
+#ARN
+print("ARN")
+#disputes barclays
+print("disputes barclays")
+#globalize
+print("globalize")
+#reverse
+print("reverse")
+#engineering
+print("engineering")
+#project shiva vishnu
+print("project shiva vishnu")
+#last updated
+print("last updated")
+#moves
+print("moves")
+#opposite
+print("opposite")
+#university
+print("university")
+#meedha pettala
+print("meedha pettala")
+#yellow
+print("yellow")
+#PK
+print("PK")
+#shiva vishnu promise
+print("shiva vishnu promise")
+#0
+print("0")
+#shiva vishnu vishnu promise
+print("shiva vishnu vishnu promise")
+#4
+print("4")
+#refer opposite SWOT
+print("refer opposite SWOT")
+#white
+print("white")
+#blue
+print("blue")
+#e
+print("e")
+#tablet
+print("tablet")
+#movies
+print("movies")
+#songs
+print("songs")
+#boolean algebra
+print("boolean algebra")
+#blue
+print("blue")
+#white
+print("white")
+#kalyan lok
+print("kalyan lok")
+#blue
+print("blue")
+#opposite
+print("opposite")
+#OR
+print("OR")
+#vishnu promise
+print("vishnu promise")
+#0=1+1
+print("0=1+1")
+#miracle
+print("miracle")
+#god
+print("god")
+#particle
+print("particle")
+#UK
+print("UK")
+#CERN
+print("CERN")
+#pick phone
+print("pick phone")
+#phone apps
+print("phone apps")
+#0
+print("0")
+#1 transaction should change the world
+print("1 transaction should change the world")
+#shiva promise
+print("#shiva promise")
+#PSO
+print("PSO")
+#2
+print("2")
+#raelian movement
+print("raelian movement")
+#swastik
+print("swastik")
+#baum
+print("baum")
+#colors
+print("colors")
+#5
+print("5")
+#red
+print("red")
+#white
+print("white")
+#blue
+print("blue")
+#yellow
+print("yellow")
+#white
+print("white")
+#blue
+print("blue")
+#green
+print("green")
+#white
+print("white")
+#blue
+print("blue")
+#AUM
+print("AUM")
+#white
+print("white")
+#baum
+print("baum")
+#use excel to filter raelian symbol
+print("use excel to filter raelian symbol")
+#01-Jul
+print("01-Jul")
+#red
+print("red")
+#PR
+print("PR")
+#6
+print("6")
+#colors
+print("colors")
+#SWOT
+print("SWOT")
+#vishnu vishnu promise
+print("vishnu vishnu promise")
+#game ends
+print("ame ends")
+#swastik
+print("swastik")
+#LVUGKE
+print("LVUGKE")
+#instead of living die/50-50 live using baum and let die
+print("instead of living die/50-50 live using baum and let die")
+#ADGRACE
+print("ADGRACE")
+#sell world
+print("sell world")
+#1+ give donation
+print("1+ give donation")
+#calculation refer to notes
+print("calculation refer to notes")
+#michelle calculation 200 200
+print("michelle calculation 200 200")
+#Journalist calculation
+print("Journalist calculation")
+#credability came into exitence
+print("credability came into exitence")
+#04:03
+print("04:03")
+#make email sarpam to hackers
+print("make email sarpam to hackers")
+#all fields
+print("all fields")
+#using boolean algebra
+print("using boolean algebra")
+#9
+print("9")
+#blue
+print("blue")
+#white
+print("white")
+#kalyan lok
+print("kalyan lok")
+#blue
+print("blue")
+
+
+# CREATION
+
+# PRESERVANCE
+
+# DESTRUCTION
+
+
+# DARK MATTER
+
+
+# LOVE
+
+# WEALTH
+
+# KNOWLEDGE
+
+
+#green
+print("green")
+#Pr
+print("Pr")
+#black
+print("black")
+#pivot table
+print("pivot table")
+#rows
+print("rows")
+#against
+print("against")
+#columns
+print("columns")
+#match
+print("match")
+#colors
+print("colors")
+#5+2(black+white)
+print("5+2(black+white)")
+#Project shiva
+print("Project shiva")
+#baum baum bole
+print("baum baum bole")
+#business 2 sides baum
+print("business 2 sides baum")
+#1 task i can do 2 tasks i am not able to link
+print("1 task i can do 2 tasks i am not able to link")
+#3=2+(1)
+print("3=2+(1)")
+#333.3.33 last excel calculation
+print("333.3.33 last excel calculation")
+#yellow
+print("yellow")
+#PK
+print("PK")
+#THE RESTORATION
+print("THE RESTORATION")
+#opposite
+print("opposite")
+#white
+print("white")
+#phone contacts
+print("phone contacts")
+#contact colors last updated
+print("contact colors last updated")
+#calls
+print("calls")
+#lappy profiles
+print("lappy profiles")
+#10 projects
+print("10 projects")
+#666+1+333
+print("666+1+333")
+#blue
+print("blue")
+#graphs
+print("graphs")
+#geographical locations
+print("geographical locations")
+#calculation
+print("calculation")
+#excel file
+print("excel file")
+#* and 1 max compatibility
+print("* and 1 max compatibility")
+#tab apps
+print("tab apps")
+#resources
+print("resources")
+#programming
+print("programming")
+#contacts
+print("contacts")
+#syncing
+print("syncing")
+#ads on profiles should change
+print("ads on profiles should change")
+#emails
+print("emails")
+#start clean
+print("start clean")
+#small zero
+print("small zero")
+#big zero
+print("big zero")
+#cern
+print("cern")
+#transaction
+print("transaction")
+#to cern
+print("to cern")
+#should give access to cern info
+print("should give access to cern info")
+#using ARN
+print("using ARN")
+#opposite
+print("opposite")
+#wiccan
+print("wiccan")
+#calendar
+print("calendar")
+#black
+print("black")
+#do what you can the game project starts
+print("do what you can the game project starts")
+#tab apps
+print("tab apps")
+#pyramids
+print("pyramids")
+#secret
+print("secret")
+#the game
+print("he game")
+#link
+print("link")
+#research symbols
+print("research symbols")
+#8
+print("8")
+#2d
+print("2d")
+#3d
+print("3d")
+#3d+2d
+print("3d+2d")
+#4
+print("4")
+#8
+print("8")
+#swastik
+print("swastik")
+#baum
+print("baum")
+#1
+print("1")
+#green
+print("green")
+#Pr
+print("Pr")
+#weigh
+print("weigh")
+#Project B-AUM
+print("Project B-AUM")
+#photos
+print("photos")
+#last updated
+print("last updated")
+#18-06-2016 18:05
+print("18-06-2016 18:05")
+#news
+print("news")
+#translate
+print("translate")
+#1
+print("1")
+#opposite
+print("opposite")
+#vishnu promise
+print("vishnu promise")
+#white
+print("white")
+#tab app
+print("tab app")
+#0
+print("0")
+#numbers
+print("numbers")
+#folders
+print("folders")
+#project
+print("project")
+#B-AUM
+print("B-AUM")
+#Sarpam
+print("Sarpam")
+#chess
+print("chess")
+#chessboard
+print("chessboard")
+#using
+print("using")
+#tic tac toe
+print("tic tac toe")
+#and
+print("and")
+#PSO
+print("PSO")
+#7
+print("7")
+#USA
+print("USA")
+#Restoration
+print("restoration")
+#the restoration of the gospel of jesus christ
+print("the restoration of the gospel of jesus christ")
+#last updated
+print("last updated")
+#moves
+print("moves")
+#opposite
+print("opposite")
+#shiva promise
+print("shiva promise")
+#university
+print("university")
+#blame
+print("blame")
+#enter the web
+print("enter the web")
+#herbalife
+print("herbalife")
+#ARN
+print("ARN")
+#disputes barclays
+print("disputes barclays")
+#globalize
+print("globalize")
+#reverse
+print("reverse")
+#engineering
+print("engineering")
+#project shiva vishnu
+print("project shiva vishnu")
+#last updated
+print("last updated")
+#moves
+print("moves")
+#opposite
+print("opposite")
+#university
+print("university")
+#meedha pettala
+print("meedha pettala")
+#yellow
+print("yellow")
+#PK
+print("PK")
+#shiva vishnu promise
+print("shiva vishnu promise")
+#0
+print("0")
+#shiva vishnu vishnu promise
+print("shiva vishnu vishnu promise")
+#4
+print("4")
+#refer opposite SWOT
+print("refer opposite SWOT")
+#white
+print("white")
+#blue
+print("blue")
+#e
+print("e")
+#tablet
+print("tablet")
+#movies
+print("movies")
+#songs
+print("songs")
+#boolean algebra
+print("boolean algebra")
+#blue
+print("blue")
+#white
+print("white")
+#kalyan lok
+print("kalyan lok")
+#blue
+print("blue")
+#opposite
+print("opposite")
+#OR
+print("OR")
+#vishnu promise
+print("vishnu promise")
+#0=1+1
+print("0=1+1")
+#miracle
+print("miracle")
+#god
+print("god")
+#particle
+print("particle")
+#UK
+print("UK")
+#CERN
+print("CERN")
+#pick phone
+print("pick phone")
+#phone apps
+print("phone apps")
+#0
+print("0")
+#1 transaction should change the world
+print("1 transaction should change the world")
+#shiva promise
+print("#shiva promise")
+#PSO
+print("PSO")
+#2
+print("2")
+#raelian movement
+print("raelian movement")
+#swastik
+print("swastik")
+#baum
+print("baum")
+#colors
+print("colors")
+#5
+print("5")
+#red
+print("red")
+#white
+print("white")
+#blue
+print("blue")
+#yellow
+print("yellow")
+#white
+print("white")
+#blue
+print("blue")
+#green
+print("green")
+#white
+print("white")
+#blue
+print("blue")
+#AUM
+print("AUM")
+#white
+print("white")
+#baum
+print("baum")
+#use excel to filter raelian symbol
+print("use excel to filter raelian symbol")
+#01-Jul
+print("01-Jul")
+#red
+print("red")
+#PR
+print("PR")
+#6
+print("6")
+#colors
+print("colors")
+#SWOT
+print("SWOT")
+#vishnu vishnu promise
+print("vishnu vishnu promise")
+#game ends
+print("ame ends")
+#swastik
+print("swastik")
+#LVUGKE
+print("LVUGKE")
+#instead of living die/50-50 live using baum and let die
+print("instead of living die/50-50 live using baum and let die")
+#ADGRACE
+print("ADGRACE")
+#sell world
+print("sell world")
+#1+ give donation
+print("1+ give donation")
+#calculation refer to notes
+print("calculation refer to notes")
+#michelle calculation 200 200
+print("michelle calculation 200 200")
+#Journalist calculation
+print("Journalist calculation")
+#credability came into exitence
+print("credability came into exitence")
+#04:03
+print("04:03")
+#make email sarpam to hackers
+print("make email sarpam to hackers")
+#all fields
+print("all fields")
+#using boolean algebra
+print("using boolean algebra")
+#9
+print("9")
+#blue
+print("blue")
+#white
+print("white")
+#kalyan lok
+print("kalyan lok")
+#blue
+print("blue")
+
+
+
+
+
+
+
+
+
+
+
+
+
